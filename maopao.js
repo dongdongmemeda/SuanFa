f536e161e8779cf291a2136c2c2927e6e7213eae
dongdongmemeda <1390916847@qq.com>
2017-10-20 22:12:25 +0800

    Update maopao.js

--- maopao.js
+++ maopao.js
@@ -8,7 +8,7 @@ function maopao(arr){
 	//  每轮i值减少一，排序比较的次数少一次，因为最后一位不用比较
 	for(let i=arr.length-1; i>=0; i--){
 		//  比较次数随着i的大小变化
-		for(let j=0; j<=i; j++){
+		for(let j=0; j<i; j++){
 			if(arr[j]>arr[j+1]){
 				const temp = arr[j]
 				arr[j] = arr[j+1]
